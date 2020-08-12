1af2d0dc9f2a7ecfa2fab893c443e20872966af8
euhenriquefranca <euhenriquefranca@gmail.com>
2020-08-12 17:02:01 -0300

    Updates

---
+++ precache-manifest.d66b85d9ea741b0667909b1ad126f263.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49ac6d0d19cea6e76970a11d0f1e22a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46e7d32e2e20b06466cf",
+    "url": "/static/css/main.87c7e3ea.chunk.css"
+  },
+  {
+    "revision": "24c185f339050365830a",
+    "url": "/static/js/2.9e89c41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9e89c41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e7d32e2e20b06466cf",
+    "url": "/static/js/main.48946ec9.chunk.js"
+  },
+  {
+    "revision": "1e967e60c8804a474105",
+    "url": "/static/js/runtime-main.9050acbe.js"
+  }
+]);
